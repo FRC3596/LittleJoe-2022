d76e609f88ead575a861fa0a533e48ab74c68169
SabrinaFajardo <sfajardo2005@gmail.com>
2022-01-14 18:32:12 -0600

    Started autonomous motors

--- src/main/java/frc/robot/subsystems/AutoModeSub.java
+++ src/main/java/frc/robot/subsystems/AutoModeSub.java
@@ -2,11 +2,14 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+import frc.robot.RobotMappings;
+
 public class AutoModeSub extends Subsystem {
     @Override
     protected void initDefaultCommand() {}
     
     public void Drive2Feet() {
-        
+        RobotMappings.driveTrainSub.SetSpeed(0.5);
+        RobotMappings.driveTrainSub.SetSpeed(0);
     }
 }
