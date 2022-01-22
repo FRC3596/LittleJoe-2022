cda62b56950ed38695be998efe6679d8aaadda8d
benjankowski <jankow.ben@gmail.com>
2022-01-21 18:55:47 -0600

    Added template commands for bot

---
+++ src/main/java/frc/robot/commands/BallLift.java
@@ -0,0 +1,12 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class BallLift extends Command {
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+    
+}
