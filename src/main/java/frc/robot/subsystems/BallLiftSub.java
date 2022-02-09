bf0dc0ef6520d601d3918383649ed9c27c1633cc
benjankowski <jankow.ben@gmail.com>
2022-02-08 19:38:57 -0600

    Added public status booleans for BallLiftSub

--- src/main/java/frc/robot/subsystems/BallLiftSub.java
+++ src/main/java/frc/robot/subsystems/BallLiftSub.java
@@ -9,6 +9,10 @@ public class BallLiftSub extends Subsystem {
     public static TalonSRX ballPuller = new TalonSRX(4);
     public static TalonSRX liftMotor = new TalonSRX(5);
 
+    public static boolean LiftStatus = false;
+    public static boolean ShootStatus = false;
+    public static boolean GatherStatus = false;
+
     @Override
     protected void initDefaultCommand() {}
 
@@ -18,6 +22,8 @@ public class BallLiftSub extends Subsystem {
         } else {
             liftMotor.set(ControlMode.PercentOutput, 0);
         }
+
+        LiftStatus = Status;
     }
 
     public void Shoot(boolean Status) {
@@ -27,6 +33,7 @@ public class BallLiftSub extends Subsystem {
             ballPuller.set(ControlMode.PercentOutput, 0);
         }
 
+        ShootStatus = Status;
     }
 
     public void Gather(boolean Status) {
@@ -35,6 +42,8 @@ public class BallLiftSub extends Subsystem {
         } else {
             ballPuller.set(ControlMode.PercentOutput, 0);
         }
+
+        GatherStatus = Status;
     }
 }
 
