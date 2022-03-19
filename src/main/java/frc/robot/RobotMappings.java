3dbdf8ba152af739f255df016c53265ab1dc76c0
benjankowski <jankow.ben@gmail.com>
2022-03-18 19:33:28 -0500

    Removed useless AssemblySub

--- src/main/java/frc/robot/RobotMappings.java
+++ src/main/java/frc/robot/RobotMappings.java
@@ -6,7 +6,6 @@ public class RobotMappings {
     public static BallLiftSub ballLiftSub = new BallLiftSub();
     public static DriveTrainSub driveTrainSub = new DriveTrainSub();
     public static AutoModeSub autoModeSub = new AutoModeSub();
-    public static BallAssemblySub ballAssemblySub = new BallAssemblySub();
     
     public static OI m_oi = new OI();
 }
