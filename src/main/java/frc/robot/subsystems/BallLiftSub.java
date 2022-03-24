145556f07aefb4a007cf5d76fe7aea7add57e954
benjankowski <jankow.ben@gmail.com>
2022-03-24 17:51:37 -0500

    Halfed motor speed for ball lift

--- src/main/java/frc/robot/subsystems/BallLiftSub.java
+++ src/main/java/frc/robot/subsystems/BallLiftSub.java
@@ -74,8 +74,8 @@ public class BallLiftSub extends Subsystem {
             liftMotorRight.set(0);
         
         } else {
-            liftMotorLeft.set(value);
-            liftMotorRight.set(value*-1);
+            liftMotorLeft.set(value/2);
+            liftMotorRight.set(value*-1/2);
         }
     }
 }
